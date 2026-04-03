29a8424dde80ac3915145dfc19fe8704be8dd168
Siro25 <si9x992005@gmail.com>
2026-04-03 13:58:39 +0700

    connect database and spring security

---
+++ src/main/java/com/example/laptopshop/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.laptopshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/")
+    public String index() {
+        return "Hello, World222!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Hello, user!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Hello, admin!";
+    }
+}

8221c62ae6cc1a31a692503ff5b427973ad0f9c1
Siro25 <si9x992005@gmail.com>
2026-05-09 23:27:16 +0700

    Spring session kết hợp rememberme

--- src/main/java/com/example/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/com/example/laptopshop/config/SecurityConfiguration.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
@@ -15,6 +17,14 @@ public class SecurityConfiguration {
         @Bean
         public PasswordEncoder passwordEncoder() {
                 return new BCryptPasswordEncoder();
+        }
+
+        @Bean
+        public SpringSessionRememberMeServices rememberMeServices() {
+                SpringSessionRememberMeServices rememberMeServices = new SpringSessionRememberMeServices();
+                rememberMeServices.setAlwaysRemember(true);
+
+                return rememberMeServices;
         }
 
         @Bean
@@ -38,12 +48,17 @@ public class SecurityConfiguration {
                                                 .successHandler(authenticationSuccessHandler)
                                                 .failureUrl("/login?error")
                                                 .permitAll())
+                                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                                                .invalidSessionUrl("/logout?expired").maximumSessions(1)
+                                                .maxSessionsPreventsLogin(false))
+                                .rememberMe(remember -> remember
+                                                .rememberMeServices(rememberMeServices()))
                                 .logout(logout -> logout
                                                 .logoutUrl("/logout")
                                                 .logoutSuccessUrl("/")
                                                 .invalidateHttpSession(true)
                                                 .clearAuthentication(true)
-                                                .deleteCookies("JSESSIONID")
+                                                .deleteCookies("JSESSIONID", "SESSION", "remember-me")
                                                 .permitAll())
 
                                 .exceptionHandling(exception -> exception
